f523e2076eea9a445ef423eddeacb6b126171199
Frostfell <ichernyshov@bk.ru>
2018-12-01 14:38:52 +0300

    Initial commit

---
+++ aviaTickets/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  aviaTickets
+//
+//  Created by Igor Chernyshov on 01/12/2018.
+//  Copyright © 2018 Igor Chernyshov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
