7497cd93b3c907211b34945f249ec9c994704eae
Frostfell <ichernyshov@bk.ru>
2018-12-02 10:22:52 +0300

    Reworked loading process

--- aviaTickets/Presentation/MainViewController.m
+++ aviaTickets/Presentation/MainViewController.m
@@ -22,10 +22,15 @@
   
   [[DataManager sharedInstance] loadData];
   
-  self.view.backgroundColor = UIColor.grayColor;
+  // Indicate data loading process on UI
+  self.view.backgroundColor = [UIColor colorWithRed:160.0/255.0
+                                              green:215.0/255.0
+                                               blue:255.0/255.0
+                                              alpha:1];
   self.activityIndicator = [self createActivityIndicator];
   [self.view addSubview:_activityIndicator];
   
+  // Create a notification subscription to know when data has been loaded
   [[NSNotificationCenter defaultCenter] addObserver:self
                                            selector:@selector(dataLoaded)
                                                name:kDataManagerLoadDataDidComplete
@@ -42,16 +47,16 @@
 
 - (void)dataLoaded
 {
+  // Refresh UI to indicate that app is ready
+  // Create a gradient
   UIColor *lightBlueColor = [UIColor colorWithRed:97.0/255.0
                                             green:215.0/255.0
                                              blue:255.0/255.0
                                             alpha:1];
-  
   UIColor *customBlueColor = [UIColor colorWithRed:72.0/255.0
                                              green:150.0/255.0
                                               blue:236.0/255.0
                                              alpha:1];
-  
   CAGradientLayer *gradient = [CAGradientLayer layer];
   gradient.frame = self.view.bounds;
   gradient.startPoint = CGPointMake(0.0, 0.0);
@@ -61,6 +66,7 @@
   [self.view.layer addSublayer:gradient];
   
   [self.activityIndicator removeFromSuperview];
+  self.navigationItem.title = @"Поиск билетов";
 }
 
 - (void)dealloc
