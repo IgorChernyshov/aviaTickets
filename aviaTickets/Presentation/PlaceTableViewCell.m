f38c9ec1e24e2cfc59949366c7720ed218022e36
Frostfell <ichernyshov@bk.ru>
2018-12-05 14:01:23 +0300

    Replaced hardcoded labels sizes to relative sizes

--- aviaTickets/Presentation/PlaceTableViewCell.m
+++ aviaTickets/Presentation/PlaceTableViewCell.m
@@ -31,16 +31,24 @@
   
   // Create labels
   _nameLabel = [UILabel new];
-  _nameLabel.frame = CGRectMake(16, 4, 300, 40);
   _nameLabel.font = [UIFont fontWithName:@"AvenirNext-Medium" size:17];
   _nameLabel.textColor = [UIColor whiteColor];
   [self.contentView addSubview:_nameLabel];
   
   _codeLabel = [UILabel new];
-  _codeLabel.frame = CGRectMake([[UIScreen mainScreen] bounds].size.width - 60, 4, 50, 40);
   _codeLabel.font = [UIFont fontWithName:@"AvenirNext-Bold" size:17];
   _codeLabel.textColor = [UIColor whiteColor];
   [self.contentView addSubview:_codeLabel];
+}
+
+- (void)layoutSubviews
+{
+  [super layoutSubviews];
+  CGFloat labelHeight = 30.0;
+  CGRect nameLabelFrame = CGRectMake(16, self.frame.size.height / 2 - labelHeight / 2, 300, labelHeight);
+  _nameLabel.frame = nameLabelFrame;
+  CGRect codeLabelFrame = CGRectMake([[UIScreen mainScreen] bounds].size.width - 60, self.frame.size.height / 2 - labelHeight / 2, 50, labelHeight);
+  _codeLabel.frame = codeLabelFrame;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
