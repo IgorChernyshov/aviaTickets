efdf57d14f3ad8c8843d7827ed798e4e269e7810
Frostfell <ichernyshov@bk.ru>
2018-12-10 16:12:17 +0300

    Created PriceMapViewController

---
+++ aviaTickets/Presentation/PriceMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  PriceMapViewController.h
+//  aviaTickets
+//
+//  Created by Igor Chernyshov on 10/12/2018.
+//  Copyright © 2018 Igor Chernyshov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PriceMapViewController : UIViewController
+
+@end
---
+++ aviaTickets/Presentation/PriceMapViewController.m
@@ -0,0 +1,22 @@
+//
+//  PriceMapViewController.m
+//  aviaTickets
+//
+//  Created by Igor Chernyshov on 10/12/2018.
+//  Copyright © 2018 Igor Chernyshov. All rights reserved.
+//
+
+#import "PriceMapViewController.h"
+
+@interface PriceMapViewController ()
+
+@end
+
+@implementation PriceMapViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+}
+
+@end
