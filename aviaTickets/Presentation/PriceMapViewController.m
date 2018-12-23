9d74c46f06d2fd3d4e0bc7cfbfe8add1e08c7990
Frostfell <ichernyshov@bk.ru>
2018-12-23 19:15:57 +0300

    Removed comments

--- aviaTickets/Presentation/PriceMapViewController.m
+++ aviaTickets/Presentation/PriceMapViewController.m
@@ -16,8 +16,6 @@
 @interface PriceMapViewController () <MKMapViewDelegate>
 
 @property (nonatomic, strong) MKMapView *mapView;
-//@property (nonatomic, strong) UIView *loadingView;
-//@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
 @property (nonatomic, strong) LocationService *locationService;
 @property (nonatomic, strong) City *origin;
 @property (nonatomic, strong) NSArray *prices;
@@ -38,27 +36,6 @@
   
   _locationService = [LocationService new];
   
-//  CGRect loadingViewFrame = CGRectMake(self.view.bounds.size.width / 2.0 - 35.0,
-//                                        self.view.bounds.size.height / 2.0 - 35.0,
-//                                        70.0,
-//                                        70.0);
-//  _loadingView = [[UIView alloc] initWithFrame: loadingViewFrame];
-//  _loadingView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.8];
-//  _loadingView.clipsToBounds = YES;
-//  _loadingView.layer.cornerRadius = 6.0;
-//  [_mapView addSubview:_loadingView];
-  
-//  CGRect activityIndicatorFrame = CGRectMake(12.5, 12.5, 50.0, 50.0);
-//  UIColor *lightBlueColor = [UIColor colorWithRed:97.0/255.0
-//                                            green:215.0/255.0
-//                                             blue:255.0/255.0
-//                                            alpha:1];
-//  _activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:activityIndicatorFrame];
-//  _activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
-//  [_activityIndicator setColor:lightBlueColor];
-//  [_activityIndicator startAnimating];
-//  [_loadingView addSubview:_activityIndicator];
-  
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateCurrentLocation:) name:kLocationServiceDidUpdateCurrentLocation object:nil];
 }
 
@@ -77,7 +54,6 @@
           [[LoadingView sharedInstance] dismiss:^{
             self.prices = prices;
           }];
-          //        [self.loadingView setHidden:YES];
         }];
       }];
     }
