f6071dc36b92ca175692ce527202b4b50added96
Frostfell <ichernyshov@bk.ru>
2018-12-26 22:04:32 +0300

    Localized TabBarController

--- aviaTickets/Presentation/TabBarController.m
+++ aviaTickets/Presentation/TabBarController.m
@@ -10,6 +10,7 @@
 #import "MainViewController.h"
 #import "PriceMapViewController.h"
 #import "SearchResultsViewController.h"
+#import "NSString+Localize.h"
 
 @interface TabBarController ()
 
@@ -32,17 +33,17 @@
   NSMutableArray<UIViewController *> *controllers = [NSMutableArray new];
   
   MainViewController *mainViewController = [MainViewController new];
-  mainViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Search Tickets" image:[UIImage imageNamed:@"searchIcon"] tag:0];
+  mainViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"searchTab".localize image:[UIImage imageNamed:@"searchIcon"] tag:0];
   UINavigationController *mainNavigationController = [[UINavigationController alloc] initWithRootViewController:mainViewController];
   [controllers addObject:mainNavigationController];
   
   PriceMapViewController *priceMapViewController = [PriceMapViewController new];
-  priceMapViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Price Map" image:[UIImage imageNamed:@"mapIcon"] tag:1];
+  priceMapViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"mapTab".localize image:[UIImage imageNamed:@"mapIcon"] tag:1];
   UINavigationController *priceMapNavigationViewController = [[UINavigationController alloc] initWithRootViewController:priceMapViewController];
   [controllers addObject:priceMapNavigationViewController];
   
   SearchResultsViewController *favoritesViewController = [[SearchResultsViewController alloc] initFavoriteTicketsController];
-  favoritesViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Favorite Tickets" image:[UIImage imageNamed:@"favoritesIcon"] tag:2];
+  favoritesViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"favoritesTab".localize image:[UIImage imageNamed:@"favoritesIcon"] tag:2];
   UINavigationController *favoritesNavigationViewController = [[UINavigationController alloc] initWithRootViewController:favoritesViewController];
   [controllers addObject:favoritesNavigationViewController];
   
