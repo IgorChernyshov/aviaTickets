ea1d8f5964109bf7ff7c4de01463d74dfb0a8a9a
Frostfell <ichernyshov@bk.ru>
2018-12-27 08:24:14 +0300

    Localized models

--- aviaTickets/Model/Country.m
+++ aviaTickets/Model/Country.m
@@ -18,8 +18,21 @@
     _translations = [dictionary valueForKey:@"name_translations"];
     _name = [dictionary valueForKey:@"name"];
     _code = [dictionary valueForKey:@"code"];
+    
+    [self localizeName];
   }
   return self;
+}
+
+- (void)localizeName {
+  if (!_translations) return;
+  NSLocale *locale = [NSLocale currentLocale];
+  NSString *localeId = [locale.localeIdentifier substringToIndex:2];
+  if (localeId) {
+    if ([_translations valueForKey: localeId]) {
+      self.name = [_translations valueForKey: localeId];
+    }
+  }
 }
 
 @end
